d915c5d165f643ebb1775401d628c0947464671d
RRV <79261047648@ya.ru>
2025-03-25 16:37:27 +0300

    app.py for day-2 practice

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from random import choice
+import sqlite3
+from pathlib import Path
+from http import HTTPStatus
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 from about import about_me
-from quotes import quotes
+
+BASE_DIR = Path(__file__).parent
+path_to_db = BASE_DIR / "store.db"
 
 app = Flask(__name__)
-app.config["JSON_AS_ASCII"] = False
+app.json.ensure_ascii = False
+
+connection = sqlite3.connect(path_to_db)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return jsonify("Hello, World!")
 
 
 @app.route("/about")
@@ -19,29 +25,101 @@ def about():
     return about_me
 
 
-# Задание 1-2
+# SELECT
 @app.route("/quotes/")
 @app.route("/quotes/<int:quote_id>")
-def quote(quote_id=None):
-    if quote_id:
-        for quote in quotes:
-            if quote["id"] == quote_id:
-                return quote
-        return f"Quote with id={quote_id} not found", 404
+def get_quote(quote_id=None):
+    select_quotes = "SELECT * FROM quotes t"
+    if quote_id is not None:
+        select_quotes += " WHERE t.id = :1"
+    connection = sqlite3.connect("store.db")
+    cursor = connection.cursor()
+    if quote_id is None:
+        cursor.execute(select_quotes)
     else:
-        return quotes
+        cursor.execute(select_quotes, {"1": quote_id} if quote_id else None)
+    quotes_db = cursor.fetchall()
+    if len(quotes_db) == 0:
+        return jsonify("Quote not found"), HTTPStatus.NOT_FOUND
+    cursor.close()
+    connection.close()
+    keys = (
+        "id",
+        "author",
+        "text",
+        "rating",
+    )
+    quotes = []
+    for quote_db in quotes_db:
+        quotes.append(dict(zip(keys, quote_db)))
+    return jsonify(quotes)
 
 
-# Задание 3
-@app.route("/count")
-def count():
-    return {"count": len(quotes)}
+def is_rating_valid(rating):
+    return 1 <= rating <= 5
 
 
-# Задание 4
-@app.route("/rand")
-def rand():
-    return quotes[choice(range(len(quotes)))]
+# INSERT
+@app.route("/quotes", methods=["POST"])
+def create_quote():
+    data = request.json
+    try:
+        connection = sqlite3.connect("store.db")
+        cursor = connection.cursor()
+        if not is_rating_valid(data["rating"]):
+            data["rating"] = 1
+        cursor.execute(
+            "INSERT INTO quotes (author, text, rating) VALUES (:author, :text, :rating)",
+            data,
+        )
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return get_quote(), HTTPStatus.CREATED
+    except KeyError:
+        return jsonify("Not enough data"), HTTPStatus.BAD_REQUEST
+
+
+# UPDATE
+@app.route("/quotes/<int:id>", methods=["PUT"])
+def edit_quote(id):
+    new_data = request.json
+    connection = sqlite3.connect("store.db")
+    cursor = connection.cursor()
+    update_str = "UPDATE quotes SET"
+    keys = new_data.keys()
+    if "author" in keys:
+        update_str += " author = '" + new_data["author"] + "',"
+    if "text" in keys:
+        update_str += " text = '" + new_data["text"] + "',"
+    if "rating" in keys:
+        update_str += " rating = " + str(new_data["rating"]) + ","
+    update_str = update_str.rstrip(",") + " WHERE id = :id"
+    cursor.execute(update_str, {"id": id})
+    connection.commit()
+    row_count = cursor.rowcount
+    cursor.close()
+    connection.close()
+    if row_count > 0:
+        return get_quote(), HTTPStatus.OK
+    else:
+        return jsonify("Quote not found"), HTTPStatus.NOT_FOUND
+
+
+# DELETE
+@app.route("/quotes/<int:id>", methods=["DELETE"])
+def delete(id):
+    connection = sqlite3.connect("store.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM quotes WHERE id = :id", {"id": id})
+    connection.commit()
+    row_count = cursor.rowcount
+    cursor.close()
+    connection.close()
+    if row_count == 0:
+        return jsonify("Quote not found"), HTTPStatus.NOT_FOUND
+    else:
+        return jsonify(f"Quote with {id=} is deleted."), HTTPStatus.OK
 
 
 if __name__ == "__main__":
