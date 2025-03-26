2f27c9d9357dca64d1543d74a87646e00a65bc3b
RRV <79261047648@ya.ru>
2025-03-26 15:47:22 +0300

    app.py for day-3 practice

--- app.py
+++ app.py
@@ -1,18 +1,58 @@
-import sqlite3
 from pathlib import Path
 from http import HTTPStatus
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
+from werkzeug.exceptions import HTTPException
+from flask_migrate import Migrate
 
 from about import about_me
 
+
+class Base(DeclarativeBase):
+    pass
+
+
 BASE_DIR = Path(__file__).parent
-path_to_db = BASE_DIR / "store.db"
 
 app = Flask(__name__)
 app.json.ensure_ascii = False
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR / 'quotes.db'}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-connection = sqlite3.connect(path_to_db)
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+
+class QuoteModel(db.Model):
+    __tablename__ = "quotes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author: Mapped[str] = mapped_column(String(32))
+    text: Mapped[str] = mapped_column(String(255))
+    rating: Mapped[int] = mapped_column(default=1)
+
+    def __init__(self, author, text, rating=None):
+        self.author = author
+        self.text = text
+        self.rating = rating
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "author": self.author,
+            "text": self.text,
+            "rating": self.rating,
+        }
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    return jsonify({"message": e.description}), e.code
 
 
 @app.route("/")
@@ -25,101 +65,96 @@ def about():
     return about_me
 
 
-# SELECT
-@app.route("/quotes/")
-@app.route("/quotes/<int:quote_id>")
-def get_quote(quote_id=None):
-    select_quotes = "SELECT * FROM quotes t"
-    if quote_id is not None:
-        select_quotes += " WHERE t.id = :1"
-    connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    if quote_id is None:
-        cursor.execute(select_quotes)
-    else:
-        cursor.execute(select_quotes, {"1": quote_id} if quote_id else None)
-    quotes_db = cursor.fetchall()
-    if len(quotes_db) == 0:
-        return jsonify("Quote not found"), HTTPStatus.NOT_FOUND
-    cursor.close()
-    connection.close()
-    keys = (
-        "id",
-        "author",
-        "text",
-        "rating",
+# GET ALL
+@app.route("/quotes")
+def get_quotes():
+    quotes = db.session.scalars(db.select(QuoteModel)).all()
+    return jsonify([quote.to_dict() for quote in quotes])
+
+
+def get_quote_by_id(quote_id):
+    return db.get_or_404(
+        QuoteModel,
+        quote_id,
+        description=f"Quote with id={quote_id} not found",
     )
-    quotes = []
-    for quote_db in quotes_db:
-        quotes.append(dict(zip(keys, quote_db)))
-    return jsonify(quotes)
+
+
+# GET ONE
+@app.route("/quotes/<int:quote_id>")
+def get_quote(quote_id):
+    quote = get_quote_by_id(quote_id)
+    return jsonify(quote.to_dict())
 
 
 def is_rating_valid(rating):
     return 1 <= rating <= 5
 
 
-# INSERT
+# POST
 @app.route("/quotes", methods=["POST"])
 def create_quote():
     data = request.json
-    try:
-        connection = sqlite3.connect("store.db")
-        cursor = connection.cursor()
-        if not is_rating_valid(data["rating"]):
-            data["rating"] = 1
-        cursor.execute(
-            "INSERT INTO quotes (author, text, rating) VALUES (:author, :text, :rating)",
-            data,
-        )
-        connection.commit()
-        cursor.close()
-        connection.close()
-        return get_quote(), HTTPStatus.CREATED
-    except KeyError:
-        return jsonify("Not enough data"), HTTPStatus.BAD_REQUEST
+    attrs = data.keys()
+    if "author" not in attrs or "text" not in attrs:
+        abort(HTTPStatus.BAD_REQUEST, "Not enough attributes")
+    author: str = ""
+    text: str = ""
+    rating: int = 1
+    for attr in attrs:
+        if attr == "author":
+            author = data[attr]
+        if attr == "text":
+            text = data[attr]
+        if attr == "rating" and is_rating_valid(data[attr]):
+            rating = int(data[attr])
+    quote = QuoteModel(
+        author,
+        text,
+        rating,
+    )
+    db.session.add(quote)
+    db.session.commit()
+    return get_quotes(), HTTPStatus.CREATED
 
 
-# UPDATE
-@app.route("/quotes/<int:id>", methods=["PUT"])
-def edit_quote(id):
-    new_data = request.json
-    connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    update_str = "UPDATE quotes SET"
-    keys = new_data.keys()
-    if "author" in keys:
-        update_str += " author = '" + new_data["author"] + "',"
-    if "text" in keys:
-        update_str += " text = '" + new_data["text"] + "',"
-    if "rating" in keys:
-        update_str += " rating = " + str(new_data["rating"]) + ","
-    update_str = update_str.rstrip(",") + " WHERE id = :id"
-    cursor.execute(update_str, {"id": id})
-    connection.commit()
-    row_count = cursor.rowcount
-    cursor.close()
-    connection.close()
-    if row_count > 0:
-        return get_quote(), HTTPStatus.OK
-    else:
-        return jsonify("Quote not found"), HTTPStatus.NOT_FOUND
+# PUT
+@app.route("/quotes/<int:quote_id>", methods=["PUT"])
+def edit_quote(quote_id):
+    data = request.json
+    attrs = set(data.keys()) & {"author", "text", "rating"}
+    if not attrs:
+        abort(HTTPStatus.BAD_REQUEST, "Nothing to update")
+    quote = get_quote_by_id(quote_id)
+    for attr in attrs:
+        if attr == "author":
+            quote.author = data[attr]
+        if attr == "text":
+            quote.text = data[attr]
+        if attr == "rating" and is_rating_valid(data[attr]):
+            quote.rating = int(data[attr])
+    db.session.commit()
+    return get_quote(quote_id)
 
 
 # DELETE
-@app.route("/quotes/<int:id>", methods=["DELETE"])
-def delete(id):
-    connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM quotes WHERE id = :id", {"id": id})
-    connection.commit()
-    row_count = cursor.rowcount
-    cursor.close()
-    connection.close()
-    if row_count == 0:
-        return jsonify("Quote not found"), HTTPStatus.NOT_FOUND
-    else:
-        return jsonify(f"Quote with {id=} is deleted."), HTTPStatus.OK
+@app.route("/quotes/<int:quote_id>", methods=["DELETE"])
+def delete(quote_id):
+    quote = get_quote_by_id(quote_id)
+    db.session.delete(quote)
+    return get_quotes()
+
+
+# FILTER
+@app.route("/filter")
+def set_filter():
+    args = request.args.to_dict()
+    attrs = args.keys() & {"author", "rating"}
+    filters = {}
+    for attr in attrs:
+        filters[attr] = args[attr]
+    quotes = db.session.scalars(db.select(QuoteModel).filter_by(**filters)).all()
+    return jsonify([quote.to_dict() for quote in quotes])
 
 
 if __name__ == "__main__":
