48df41301fc9825cb7b1def94c5e86266de50216
RRV <79261047648@ya.ru>
2025-03-26 15:48:57 +0300

    create db-schema

---
+++ sqla_migrate.py
@@ -0,0 +1,23 @@
+from app import app, db, QuoteModel
+from quotes import quotes
+
+with app.app_context():
+    db.create_all()
+
+q1 = QuoteModel("Народная мудрость", "Нет пламя без огня")
+with app.app_context():
+    db.session.add(q1)
+    for quote in quotes:
+        db.session.add(
+            QuoteModel(
+                quote["author"],
+                quote["text"],
+                quote["rating"],
+            )
+        )
+    db.session.commit()
+
+
+with app.app_context():
+    quotes = db.session.scalars(db.select(QuoteModel)).all()
+    print(quotes)
